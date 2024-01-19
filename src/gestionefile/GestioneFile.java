c6cbe82508bd69d087086c3dd915a23db376f0c2
Matty0five <matteo.mazzieri.05@gmail.com>
2024-01-19 15:50:44 +0100

    Primo commit

---
+++ src/gestionefile/GestioneFile.java
@@ -0,0 +1,49 @@
+package gestionefile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Matteo Mazzieri
+ * @version 19/01/24
+ */
+public class GestioneFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //1)LETTURA
+        Lettore lettore = new Lettore("C:\\Users\\matte\\OneDrive\\Scuola superiore\\Quinto anno\\TPSIT\\Progetti\\Gestione file\\GestioneFile\\user.json");
+        lettore.start();
+        try{
+            lettore.join();
+        } catch (InterruptedException ex) {
+            System.err.println("Errore in lettura" + ex.getMessage());
+        }
+        //2)ELABORAZIONE
+
+        String username = null;
+        String password = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.print("Inserisci l'username: ");
+            username = br.readLine().toUpperCase();
+            System.out.print("Inserisci la password: ");
+                password = br.readLine().toUpperCase();
+        } catch (IOException e) {
+            System.err.println("Errore durante la lettura dell'input: " + e.getMessage());
+        }
+
+        Cifratore cifratore=new Cifratore("VERME");
+        
+        //3) SCRITTURA
+        Scrittore scrittore = new Scrittore("output.csv", username, cifratore.cifra(password));
+        Thread threadScrittore = new Thread(scrittore);
+        threadScrittore.start();
+    }
+    
+}
