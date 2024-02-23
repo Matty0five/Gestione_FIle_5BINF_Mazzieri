678fd35f06fe11c555eb532d0c124c562e5870c3
Matty0five <matteo.mazzieri.05@gmail.com>
2024-02-23 22:54:43 +0100

    ISSUE #2 risolta

--- src/gestionefile/Lettore.java
+++ src/gestionefile/Lettore.java
@@ -22,18 +22,14 @@ public class Lettore extends Thread{
      */
     public String leggi(boolean stampa){
         String contenuto = "";
-        FileReader fr;
         int i; 
-        try { 
-            //1) apro il file
-            fr = new FileReader(nomeFile);
+        try(FileReader fr = new FileReader(nomeFile)){
+
             //2) leggo carattere per carattere e lo stampo 
             while ((i=fr.read()) != -1)
                 contenuto += (char) i;
             
             System.out.print("\n\r");
-            //3) chiudo il file
-            fr.close();
         } catch (IOException ex) {
             System.err.println("Errore in lettura!");
         }
--- src/gestionefile/Scrittore.java
+++ src/gestionefile/Scrittore.java
@@ -32,30 +32,17 @@ public class Scrittore implements Runnable{
      * Scrive un file di testo usando la classe BufferedWriter
      */
     public void scrivi(){
-        BufferedWriter br=null;
         
-        try {
-            //1) apro il file
-            br = new BufferedWriter(new FileWriter(nomeFile));
+        try(BufferedWriter br = new BufferedWriter(new FileWriter(nomeFile))){
+
             //2) scrivo nel buffer
             br.write(username);
             br.write(";");
             br.write(password);
             br.write("\n\r");
-            //3) svuoto il buffer e salvo nel file i dati
-            br.flush();
+            
         } catch (IOException ex) {
             Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        finally{
-            if (br!=null)
-                try {
-                    //4)chiudo lo stream in uscita
-                    br.close();
-            } catch (IOException ex) {
-                Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
-            }
-                
         }
     }
 }
